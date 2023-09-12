8632cb85debfb9227c420ae73ca8035150cef283
Dani-Ideas <daniideas0@gamil.com>
2023-09-11 21:24:45 -0600

    el metodo imprimirElemetos ya no cae en un bucle infinito

--- src/fes/aragon/utilerias/dinamicas/listasimple/ListaSimpleCircular.java
+++ src/fes/aragon/utilerias/dinamicas/listasimple/ListaSimpleCircular.java
@@ -1,6 +1,20 @@
 package fes.aragon.utilerias.dinamicas.listasimple;
 
-public class ListaSimpleCircular<E> extends ListaSimple<Object>{
+public class ListaSimpleCircular<E> extends ListaSimple<E>{
+	
+	
+	public void imprimirElemetos() {
+	    Nodo<E> temporal = header;
+	    int count = 0;
+	    while (temporal != null && count < longitud) {
+	        System.out.println(temporal.getData());
+	        temporal = temporal.getNext();
+	        count++;
+	        if (temporal == header) {
+	            break;
+	        }
+	    }
+	}
 	public boolean transfomacionCircular() {
 		if (header.equals(null))
 			return false;
