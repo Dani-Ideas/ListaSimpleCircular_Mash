974eb88106493d822f973f2a05591c7911c374dc
Dani-Ideas <daniideas0@gamil.com>
2023-09-17 15:31:17 -0600

    Esta es la vesion que simplemete imprime Mash

--- src/Herramintas/Herramientas.java
+++ src/Herramintas/Herramientas.java
@@ -1,22 +1,17 @@
 package Herramintas;
 
 import java.util.Random;
-
-import fes.aragon.utilerias.dinamicas.listasimple.ListaSimple;
 import fes.aragon.utilerias.dinamicas.listasimple.ListaSimpleCircular;
 
 public class Herramientas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ListaSimpleCircular<Integer> lista1=new ListaSimpleCircular<Integer>();
+		ListaSimpleCircular<Character> lista1=new ListaSimpleCircular<Character>();
 		//ListaSimple<Integer> lista2=new ListaSimple<>();
-		Random numeberRandomGenerate= new Random();
-		int contador=0;
-		for (int i=0; i<3; i++) {
-			int numberRandom= numeberRandomGenerate.nextInt(50,121);
-			//lista2.agregarEnCabeza(numberRandom);
-			lista1.agregarEnCola(numberRandom);
+		String name="Smash";
+		for (int i=0; i<name.length(); i++) {
+			lista1.agregarEnCola(name.charAt(i));
 		}
 		lista1.imprimirElemetos();
 		
@@ -24,17 +19,6 @@ public class Herramientas {
 		System.out.println(lista1.transfomacionCircular());
 		System.out.println("--------------------------\n \n \n \n \n");
 		
-		while (lista1.transfomacionCircular()&&contador <=3) {
-			System.out.println("Se viene en "+(3-contador)+":\n(づ｡◕‿‿◕｡)づ(づ｡◕‿‿◕｡)づ(づ｡◕‿‿◕｡)づ(づ｡◕‿‿◕｡)づ(づ｡◕‿‿◕｡)づ\n( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)");
-            try {
-            	Thread.sleep(5000); 
-            } catch (InterruptedException e) {
-                
-                e.printStackTrace();
-            }
-            
-            contador++;
-        }
 		lista1.imprimirElemetos();
 		
 		//lista2.imprimirElemetos();
